2128d1f513ec15d41480017a6d3dc0b8f400893c
Adarsh Panchal <adarshpanchal047@gmail.com>
2026-03-28 01:26:24 +0530

    commit to github

---
+++ script.js
@@ -0,0 +1,14 @@
+signin_button = document.querySelector(".signin")
+signin_button.addEventListener("click", () => {
+    alert("This feature coming soon")
+})
+
+getstarted = document.querySelector(".getstarted")
+getstarted.addEventListener("click",()=>{
+    alert("your computer is hacked mf");
+})
+
+getstarted2 = document.querySelector(".getstarted2")
+getstarted2.addEventListener("click",()=>{
+    alert("your computer is hacked mf");
+})
